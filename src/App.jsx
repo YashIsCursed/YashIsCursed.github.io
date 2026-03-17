4931eac9747f9a73ef57596a3157709bc22e6fbc
yashbokade2007@gmail.com <yashbokade2007@gmail.com>
2026-03-17 19:55:25 +0530

    Implementing About

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
 import {useRef, useState} from "react";
+
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
 import {ScrollTrigger,SplitText,Observer} from "gsap/all";
+
+import {Menu} from "lucide-react";
 function App() {
     const NameT = useRef(null);
     const linkList = useRef(null);
@@ -27,37 +30,68 @@ function App() {
                 })
             }
         })
-
         gsap.from('.links',{
 
             y:"-101%",
-            stagger: 0.125,
+            stagger: 0.25,
             delay:3
         })
     })
 
   return (
-    <>
-        <div className="flex sm:flex-col flex-row w-full justify-between items-center">
-            <div className="overflow-hidden">
-                <h1 ref={NameT}>Yash Bokade</h1>
-            </div>
-            <div className="relative">
-                <button
-                    className="lg:hidden text-[32px]"
-                    onClick={()=>{setMenu(!menu)}}>⇶</button>
-                <ul
-                    ref={linkList}
-                    className={`h-fit ${menu?'flex bg-[#A3A3D310] py-4 px-8 rounded-xl absolute right-full top-[120%]':'hidden'} lg:flex flex-col lg:flex-row gap-6 overflow-y-hidden px-2 text-xl`}>
-                    <li className="links">About</li>
-                    <li className="links">Projects</li>
-                    <li className="links">Contact</li>
-                    <li className="links">Resume/CV</li>
-                </ul>
-            </div>
-        </div>
-        <div><h2>Hello World</h2></div>
-    </>  )
+      <>
+          <div className="w-full h-screen my-8 flex flex-col gap-12">
+              <div className="flex flex-row w-full justify-between items-center">
+                    <div className="overflow-hidden text-2xl">
+                        <h1 ref={NameT}>Yash Bokade</h1>
+                    </div>
+                    <div className="relative">
+                        <div
+                            className="lg:hidden text-[32px]"
+                            onClick={()=>{
+                                gsap.from(".links",{
+                                    y:"-200%",
+                                    stagger: 0.125,
+                                    duration:0.5,
+                                })
+                                setMenu(!menu)
+                            }}>
+                            <Menu />
+                        </div>
+                        <ul
+                            ref={linkList}
+                            className={`h-fit ${menu?'flex bg-[#1f1f1f55] backdrop-blur-2xl  py-4 px-8 rounded-xl absolute right-full top-[120%]':'hidden'} lg:flex flex-col lg:flex-row gap-6 overflow-y-hidden px-2 text-xl`}>
+                            <li
+                                onClick={(e)=>{
+                                    window.location.href = "/projects";
+                                    }}
+                                className="links"><p href="/projects">About</p></li>
+                            <li
+                                onClick={(e)=>{
+                                    window.location.href = "/projects";
+                                    }}
+                                className="links"><p>Projects</p></li>
+                            <li
+                                onClick={(e)=>{
+                                    window.location.href = "/projects";
+                                    }}
+                                className="links"><p>Contact</p></li>
+                            <li
+                                onClick={(e)=>{
+                                    window.location.href = "/projects";
+                                    }}
+                                className="links"><p>Resume</p></li>
+                        </ul>
+                    </div>
+              </div>
+              <div>
+                  <p className="text-[16px]">About</p>
+                  <div id={"About"} className="w-full h-screen">
+                      About
+                  </div>
+              </div>
+          </div>
+      </>)
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,18 @@ import Loading from "./components/Loading.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Loading/>
-        <App />
+        {window.location.pathname === "/" && <>
+            <Loading/>
+            <App />
+        </>}
+        {window.location.pathname === "/projects" && <div>hello Path <p onClick={(e)=>{
+            e.preventDefault();
+            window.history.back();
+        }}>Back</p></div>}
+
+        {window.location.pathname !== "/" && <div>hello Path <p onClick={(e)=>{
+            e.preventDefault();
+            window.history.back();
+        }}>Back</p></div>}
     </StrictMode>,
 )
